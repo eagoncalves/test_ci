33372ff0e12f52b115034b7190f553efcd76c314
Mac <mac@Macs-Mac-mini.local>
2024-11-18 20:12:56 -0300

    failed test

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	total := Soma(15, 1)
+	total := Soma(15, 10)
 
 	if total != 30 {
 		t.Errorf("Resultado da soma é invalido: %d / Resultado esperado: %d", total, 30)
